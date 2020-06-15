5fe5025c17d59d07dcfd0b99df8d9c5e10cf169a
Niall King <n.king@syzygy.co.uk>
2020-06-15 18:47:24 +0100

    add stereo dice sounds

--- src/components/dice/die.js
+++ src/components/dice/die.js
@@ -20,8 +20,6 @@ import { DICE_SIDES } from '../../constants';
 class Dice extends Component {
   constructor(props){
     super(props);
-    this.audioRef1 = React.createRef();
-    this.audioRef2 = React.createRef();
     this.state = {
         value: 0
     }
@@ -40,67 +38,58 @@ class Dice extends Component {
     }
   }
 
-  componentDidUpdate() {
-    if (this.props.rolling) {
-
-    }
-  }
-
   render() {
     const {rolling, player} = this.props;
     const {value} = this.state;
 
-    return (
-    <>
-      { rolling && <>
-        <audio ref={this.audioRef1} src={`${process.env.PUBLIC_URL}/sounds/dice/dice-1.mp3`} autoPlay />
-        <audio ref={this.audioRef2} src={`${process.env.PUBLIC_URL}/sounds/dice/dice-2.mp3`} autoPlay />
-        </>
+    return (<>
+      { 
+        rolling && <audio src={`${process.env.PUBLIC_URL}/sounds/dice/dice-stereo.mp3`} autoPlay />
       }
-        <div className={`die ${player} ${rolling ? 'rolling': ''} value-${value}`}>
-      <div className="side front">
-        <div className="dot center"></div>
+      <div className={`die ${player} ${rolling ? 'rolling': ''} value-${value}`}>
+        <div className="side front">
+          <div className="dot center"></div>
+        </div>
+        <div className="side front inner"></div>
+        <div className="side top">
+          <div className="dot dtop dleft"></div>
+          <div className="dot dbottom dright"></div>
+        </div>
+        <div className="side top inner"></div>
+        <div className="side right">
+          <div className="dot dtop dleft"></div>
+          <div className="dot center"></div>
+          <div className="dot dbottom dright"></div>
+        </div>
+        <div className="side right inner"></div>
+        <div className="side left">
+          <div className="dot dtop dleft"></div>
+          <div className="dot dtop dright"></div>
+          <div className="dot dbottom dleft"></div>
+          <div className="dot dbottom dright"></div>
+        </div>
+        <div className="side left inner"></div>
+        <div className="side bottom">
+          <div className="dot center"></div>
+          <div className="dot dtop dleft"></div>
+          <div className="dot dtop dright"></div>
+          <div className="dot dbottom dleft"></div>
+          <div className="dot dbottom dright"></div>
+        </div>
+        <div className="side bottom inner"></div>
+        <div className="side back">
+          <div className="dot dtop dleft"></div>
+          <div className="dot dtop dright"></div>
+          <div className="dot dbottom dleft"></div>
+          <div className="dot dbottom dright"></div>
+          <div className="dot center dleft"></div>
+          <div className="dot center dright"></div>
+        </div>
+        <div className="side back inner"></div>
+        <div className="side cover x"></div>
+        <div className="side cover y"></div>
+        <div className="side cover z"></div>
       </div>
-      <div className="side front inner"></div>
-      <div className="side top">
-        <div className="dot dtop dleft"></div>
-        <div className="dot dbottom dright"></div>
-      </div>
-      <div className="side top inner"></div>
-      <div className="side right">
-        <div className="dot dtop dleft"></div>
-        <div className="dot center"></div>
-        <div className="dot dbottom dright"></div>
-      </div>
-      <div className="side right inner"></div>
-      <div className="side left">
-        <div className="dot dtop dleft"></div>
-        <div className="dot dtop dright"></div>
-        <div className="dot dbottom dleft"></div>
-        <div className="dot dbottom dright"></div>
-      </div>
-      <div className="side left inner"></div>
-      <div className="side bottom">
-        <div className="dot center"></div>
-        <div className="dot dtop dleft"></div>
-        <div className="dot dtop dright"></div>
-        <div className="dot dbottom dleft"></div>
-        <div className="dot dbottom dright"></div>
-      </div>
-      <div className="side bottom inner"></div>
-      <div className="side back">
-        <div className="dot dtop dleft"></div>
-        <div className="dot dtop dright"></div>
-        <div className="dot dbottom dleft"></div>
-        <div className="dot dbottom dright"></div>
-        <div className="dot center dleft"></div>
-        <div className="dot center dright"></div>
-      </div>
-      <div className="side back inner"></div>
-      <div className="side cover x"></div>
-      <div className="side cover y"></div>
-      <div className="side cover z"></div>
-    </div>
      </>
     )
   }
